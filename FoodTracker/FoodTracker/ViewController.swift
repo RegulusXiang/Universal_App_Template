2f8acb5df574884beb61e764a4ced0cc068ecdd9
RegulusXiang <regulus_xiang@yeah.net>
2016-01-02 06:33:19 -0800

    FoodTracker
    Learning to use Swift language to develop a simple application.

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by xg on 16/1/2.
+//  Copyright © 2016年 xg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //I have no need to implement this method.
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+}
+
