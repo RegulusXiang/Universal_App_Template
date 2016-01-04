ed5f50f0d6aea49a3d54f0839022d95d6cdef582
RegulusXiang <regulus_xiang@yeah.net>
2016-01-04 05:48:47 -0800

    Work with View Controllers
    Add an Image View and let it recognize tap gestures from user.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {// adopt the protocol for UITextFieldDelegate
+class ViewController: UIViewController, UITextFieldDelegate,
+                      UIImagePickerControllerDelegate, UINavigationControllerDelegate
+    {// adopt the protocol for UITextFieldDelegate
 
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField! //IB for interface builder, weak means it can be nil, 
@@ -16,6 +18,8 @@ class ViewController: UIViewController, UITextFieldDelegate {// adopt the protoc
     //optional type that will always have a value after the value is first set.
     
     @IBOutlet weak var mealNameLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +39,44 @@ class ViewController: UIViewController, UITextFieldDelegate {// adopt the protoc
         mealNameLabel.text = "Default Text"
     }
     
+    //it’s quite easy to give a view the same capabilities as a control
+    //by adding a gesture recognizer to it.
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        //Hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        //Only allow image to be picked, not taken
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        
+    }
+    
     
     //MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -48,6 +90,8 @@ class ViewController: UIViewController, UITextFieldDelegate {// adopt the protoc
         mealNameLabel.text = textField.text
         
     }
+    
+   
     
 }
 
