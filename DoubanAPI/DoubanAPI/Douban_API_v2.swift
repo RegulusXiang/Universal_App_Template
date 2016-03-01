436b3e23caa7ac4c14ac19a20b9fb5bec26bcf3b
RegulusXiang <RegulusXiang@koukontekiMacBook-Air.local>
2016-03-01 21:10:31 +0800

    豆瓣API
    doubt api

---
+++ DoubanAPI/DoubanAPI/Douban_API_v2.swift
@@ -0,0 +1,26 @@
+//
+//  Douban_API_v2.swift
+//  DoubanAPI
+//
+//  Created by 向根 on 16/2/2.
+//  Copyright © 2016年 向根. All rights reserved.
+//
+
+import Foundation
+
+class test
+{
+    //搜索豆瓣图书
+    func searchBook(){
+        let urlPath = "https://api.douban.com/v2/book/search?q=小王子"
+        let url = NSURL(string:urlPath)
+        
+        
+    }
+    
+    
+    
+    func testfunc(){
+        print("test")
+    }
+}
